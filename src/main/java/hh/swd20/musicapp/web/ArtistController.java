b024b20dd020fbe702e678ed04e865f82458f45e
Sofia Lumme <sofia.lumme@gmail.com>
2022-04-17 00:41:13 +0300

    added bootstrap css and index html template

--- src/main/java/hh/swd20/musicapp/web/ArtistController.java
+++ src/main/java/hh/swd20/musicapp/web/ArtistController.java
@@ -2,6 +2,7 @@ package hh.swd20.musicapp.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import hh.swd20.musicapp.domain.ArtistRepository;
 
@@ -10,5 +11,10 @@ public class ArtistController {
 	
 	@Autowired
 	private ArtistRepository artistRepository;
+	
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
 
 }
